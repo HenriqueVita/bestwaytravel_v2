cb7900d0a4ab4b5638084754d6b642b29ef06888
Henrique VIta Velloso <hvita33@gmail.com>
2025-08-07 11:05:35 -0300

    Ajuste de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,35 +11,16 @@ const routes: Routes = [
     path: '',
     component: AdminComponent,
     children: [
-      {
-        path: '',
-        redirectTo: '/home',
-        pathMatch: 'full'
-      },
-      {
-        path: 'home',
-        loadChildren: () => import('./features/dashboard/dashboard-module').then((c) => c.DashboardModule)
-      },
-      {
-        path: 'orcamentos',
-        loadChildren: () => import('./features/orcamentos/orcamentos-module').then((m) => m.OrcamentosModule)
-      },
-      {
-        path: 'chart',
-        loadComponent: () => import('./demo/chart-maps/core-apex.component').then((c) => c.CoreApexComponent)
-      },
-      {
-        path: 'forms',
-        loadComponent: () => import('./demo/forms/form-elements/form-elements.component').then((c) => c.FormElementsComponent)
-      },
-      {
-        path: 'tables',
-        loadComponent: () => import('./demo/tables/tbl-bootstrap/tbl-bootstrap.component').then((c) => c.TblBootstrapComponent)
-      },
-      {
-        path: 'sample-page',
-        loadComponent: () => import('./demo/other/sample-page/sample-page.component').then((c) => c.SamplePageComponent)
-      }
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: 'home', loadChildren: () => import('./features/dashboard/dashboard-module').then((c) => c.DashboardModule) },
+      { path: 'orcamentos', loadChildren: () => import('./features/orcamentos/orcamentos-module').then((m) => m.OrcamentosModule) },
+      { path: 'vendas', loadChildren: () => import('./features/vendas/vendas-module').then(m => m.VendasModule) },
+      { path: 'financeiro', loadChildren: () => import('./features/financeiro/financeiro-module').then(m => m.FinanceiroModule) },
+      { path: 'clientes', loadChildren: () => import('./features/clientes/clientes-module').then(m => m.ClientesModule) },
+      { path: 'faturas', loadChildren: () => import('./features/faturas/faturas-module').then(m => m.FaturasModule) },
+      { path: 'pagamentos', loadChildren: () => import('./features/pagamentos/pagamentos-module').then(m => m.PagamentosModule) },
+      { path: 'usuarios', loadChildren: () => import('./features/usuarios/usuarios-module').then(m => m.UsuariosModule) },
+      { path: 'destinos', loadChildren: () => import('./features/destinos/destinos-module').then(m => m.DestinosModule) }
     ]
   },
   {
@@ -56,13 +37,7 @@ const routes: Routes = [
       }
     ]
   },
-  { path: 'vendas', loadChildren: () => import('./features/vendas/vendas-module').then(m => m.VendasModule) },
-  { path: 'financeiro', loadChildren: () => import('./features/financeiro/financeiro-module').then(m => m.FinanceiroModule) },
-  { path: 'clientes', loadChildren: () => import('./features/clientes/clientes-module').then(m => m.ClientesModule) },
-  { path: 'faturas', loadChildren: () => import('./features/faturas/faturas-module').then(m => m.FaturasModule) },
-  { path: 'pagamentos', loadChildren: () => import('./features/pagamentos/pagamentos-module').then(m => m.PagamentosModule) },
-  { path: 'usuarios', loadChildren: () => import('./features/usuarios/usuarios-module').then(m => m.UsuariosModule) },
-  { path: 'destinos', loadChildren: () => import('./features/destinos/destinos-module').then(m => m.DestinosModule) }
+  
 ];
 
 @NgModule({
--- src/app/features/financeiro/financeiro-routing-module.ts
+++ src/app/features/financeiro/financeiro-routing-module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from '../dashboard/dashboard';
-import { CadastroLancamentoComponent } from './lancamentos/cadastro-lancamento/cadastro-lancamento';
-import { ListaLancamentosComponent } from './lancamentos/lista-lancamentos/lista-lancamentos';
-import { FluxoCaixaComponent } from './relatorios/fluxo-caixa/fluxo-caixa.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'lancamentos', component: ListaLancamentosComponent },
-  { path: 'lancamentos/novo', component: CadastroLancamentoComponent },
-  { path: 'relatorios/fluxo-caixa', component: FluxoCaixaComponent },
+  { path: 'dashboard', loadComponent: () => import('./dashboard/dashboard').then((c) => c.Dashboard)  },
+  { path: 'lancamentos', loadComponent: () => import('./lancamentos/lista-lancamentos/lista-lancamentos').then((c) => c.ListaLancamentosComponent) },
+  { path: 'lancamentos/novo', loadComponent: () => import('./lancamentos/cadastro-lancamento/cadastro-lancamento').then((c) => c.CadastroLancamentoComponent) },
+  { path: 'relatorios/fluxo-caixa', loadComponent: () => import('./relatorios/fluxo-caixa/fluxo-caixa.component').then((c) => c.FluxoCaixaComponent) },
 ];
 
 @NgModule({
